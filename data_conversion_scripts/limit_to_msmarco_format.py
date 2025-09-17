8620848f410bd5cd9cf3566ac2ee0ec963e1cf53
adrianmbracher <adrian.bracher@live.at>
2025-09-17 15:24:33 +0200

    fix paths

--- data_conversion_scripts/limit_to_msmarco_format.py
+++ data_conversion_scripts/limit_to_msmarco_format.py
@@ -3,19 +3,19 @@ import json
 
 if __name__ == "__main__":
     # Convert limit queries to msmarco tsv format
-    with open("../limit/limit/queries.jsonl", "r") as infile, open("../limit_formatted/limit/queries/raw.tsv", "x") as outfile:
+    with open("./limit/limit/queries.jsonl", "r") as infile, open("./limit_formatted/limit/queries/raw.tsv", "x") as outfile:
         for line in infile:
             data = json.loads(line)
             outfile.write(f"{data['_id']}\t{data['text']}\n")
 
     # Convert limit corpus to msmarco tsv format
-    with open("../limit/limit/corpus.jsonl", "r") as infile, open("../limit_formatted/limit/corpus/raw.tsv", "x") as outfile:
+    with open("./limit/limit/corpus.jsonl", "r") as infile, open("./limit_formatted/limit/corpus/raw.tsv", "x") as outfile:
         for line in infile:
             data = json.loads(line)
             outfile.write(f"{data['_id']}\t{data['text']}\n")
 
     # Convert limit qrels to msmarco tsv format
-    with open("../limit/limit/qrels.jsonl", "r") as infile, open("../limit_formatted/limit/queries/qrel.json", "x") as outfile:
+    with open("./limit/limit/qrels.jsonl", "r") as infile, open("./limit_formatted/limit/queries/qrel.json", "x") as outfile:
         qrels = {}
         for line in infile:
             data = json.loads(line)
