a850aa03833862b9614b616df4c31bc2d0b233dc
adrianmbracher <adrian.bracher@live.at>
2025-09-29 14:43:38 +0200

    try to move local rank from args to environment variables

--- t5_pretrainer/main.py
+++ t5_pretrainer/main.py
@@ -112,9 +112,9 @@ def initialize_splade_evaluator(model, args, model_args):
 def main():
     parser = HfArgumentParser((ModelArguments, Arguments))
     model_args, args = parser.parse_args_into_dataclasses()
-    args.local_rank = int(os.environ['LOCAL_RANK'])
+    local_rank = int(os.environ['LOCAL_RANK'])
     # save args to disk 
-    if args.local_rank <= 0:
+    if local_rank <= 0:
         merged_args = {**asdict(model_args), **asdict(args)}
         out_dir = deepcopy(args.output_dir)
         if not os.path.exists(out_dir):
