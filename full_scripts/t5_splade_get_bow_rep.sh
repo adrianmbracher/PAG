0c49305d67c10c17c8226682edd34bb4f6b0726f
adrianmbracher <adrian.bracher@live.at>
2025-09-30 09:21:50 +0200

    change hardcoded paths to limit

--- full_scripts/t5_splade_get_bow_rep.sh
+++ full_scripts/t5_splade_get_bow_rep.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-collection_path=./data/msmarco-full/full_collection/
+collection_path=./limit_formatted/limit/corpus
 experiment_dir=experiments-splade
 
 export CUDA_VISIBLE_DEVICES=0
-model_dir="./data/$experiment_dir/t5-splade-0-12l"
+model_dir="./limit-formatted/$experiment_dir/t5-splade-0-12l"
 pretrained_path=$model_dir/checkpoint
 
 out_dir=$model_dir/top_bow
